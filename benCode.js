6cc28d3269ce0a206c6dbf8ddad7962d8234e1a6
Simon White <simon@trigowhite.com>
2016-10-01 16:21:20 +0100

    tidy up

---
+++ benCode.js
@@ -0,0 +1,8 @@
+// Ben's code. Written in JavaScript language.
+
+
+function double(x) {
+
+    return x * 2;
+
+}
--- code.js
+++ code.js
@@ -1,30 +1,8 @@
-// Code for the page - the functionality. Written in JavaScript language.
-
+// Code for the page. Written in JavaScript language.
 
 $(document).ready(runPage);
 
-
 function runPage() {
-
-/*
-    setupQuestion( {
-        trueButton: $("#trueButton"),
-        falseButton: $("#falseButton"),
-        submitButton: $("#submitButton"),
-        
-        nextBit: $("#question2"),
-        cookieName: "answeredQ1"
-    } );
-
-    setupQuestion( {
-        trueButton: $("#trueButto"),
-        falseButton: $("#falseButto"),
-        submitButton: $("#submitButto"),
-        
-        nextBit: $("#question3"),
-        cookieName: "answeredQ2"
-    } );
-*/
 
     addClick($("#submitButton"), submitClick);
     addClick($("#startAgain"), startAgainClick);
@@ -36,93 +14,13 @@ function submitClick() {
 
     var output = double(input);
 
-
     $("#outputField").text(output);
 
     $("#output").removeClass("before");
     $("#output").addClass("after");        
-
-//    removeClick($("#submitButton"));
-
-//    elements.submitButton.hide();
-
 }
-
-function double(x) {
-
-    return x * 2;
-
-}
-
-function setupQuestion(elements) {
-
-    if (getCookie(elements.cookieName)) {
-
-        selectTrueOrFalse();
-
-        elements.submitButton.hide();
-    }
-    else {
-
-        addClick(elements.trueButton, trueClick);
-        addClick(elements.falseButton, falseClick);
-
-        elements.nextBit.hide();
-    }
-
-    function trueClick() {
-
-        elements.trueButton.addClass("selected");
-        elements.falseButton.removeClass("selected");
-
-        addClick(elements.submitButton, submitClick);
-    }
-
-    function falseClick() {
-
-        elements.trueButton.removeClass("selected");
-        elements.falseButton.addClass("selected");
-
-        addClick(elements.submitButton, submitClick);
-    }
-
-    function submitClick() {
-
-        rememberTrueOrFalse();
-        
-        removeClick(elements.trueButton);
-        removeClick(elements.falseButton);
-        removeClick(elements.submitButton);
-
-        elements.submitButton.hide();
-
-        elements.nextBit.show("slow");
-    }
-    
-    function selectTrueOrFalse() {
-        
-        if (getCookie(elements.cookieName) == "true")
-            elements.trueButton.addClass("selected");
-
-        else if (getCookie(elements.cookieName) == "false")
-            elements.falseButton.addClass("selected");
-    }
-
-    function rememberTrueOrFalse() {
-        
-        if (elements.trueButton.hasClass("selected"))
-            setCookie(elements.cookieName, "true");
-
-        else if (elements.falseButton.hasClass("selected"))
-            setCookie(elements.cookieName, "false");
-    }
-}
-
 
 function startAgainClick() {
-
-    resetCookie("answeredQ1");
-    resetCookie("answeredQ2");
 
     location.reload(false);
 }
